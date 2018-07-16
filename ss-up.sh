e74ed55f281393a10c85d13c3635cb94839eca8a
ouchl <leonhao2014@gmail.com>
2018-07-17 00:39:53 +0800

    Create ss-up.sh

---
+++ ss-up.sh
@@ -0,0 +1,36 @@
+ipset -N chnroute hash:net maxelem 65536
+
+for ip in $(cat '/etc/chnroute.txt'); do
+  ipset add chnroute $ip
+done
+
+iptables -t nat -N SHADOWSOCKS
+
+# 直连服务器 IP
+iptables -t nat -A SHADOWSOCKS -d 35.236.173.130/24 -j RETURN
+
+# 允许连接保留地址
+iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+
+# 直连中国 IP
+iptables -t nat -A SHADOWSOCKS -p tcp -m set --match-set chnroute dst -j RETURN
+iptables -t nat -A SHADOWSOCKS -p icmp -m set --match-set chnroute dst -j RETURN
+
+# 重定向到 ss-redir 端口
+iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-port 1080
+
+# Add any UDP rules
+ip route add local default dev lo table 100
+ip rule add fwmark 1 lookup 100
+iptables -t mangle -A SHADOWSOCKS -p udp --dport 53 -j TPROXY --on-port 1080 --tproxy-mark 0x01/0x01
+
+# Apply the rules
+iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS
+iptables -t mangle -A PREROUTING -j SHADOWSOCKS
